65c58b05f0ae8335031e0965d8243d593b077d87
Gabriel F. Nobre <nobrecoder@gmail.com>
2021-10-18 15:54:36 -0300

    18/10/2021

--- arquivos_das_aulas/034-manipulando_Objects.js
+++ arquivos_das_aulas/034-manipulando_Objects.js
@@ -48,3 +48,50 @@ console.log('\nNome do produto 2: ', prod2.nome);
 console.log('Preço do produto 2: ', prod2.preco);
 console.log('Cor do produto 2: ', prod2.estilo.cor); //Se a chave possuí uma subchave usamos a notação ponto mais uma vez...
 console.log('Tamanhos do produto 2: ', prod2.estilo.tamanhos); //Chamando todos os valores de uma sub-chave...
+
+
+
+//DECLARANDO UM OBJECT USANDO A FUNÇÃO OBJECT:
+let produto = new Object; //Veja que podemos usar a função interna Object do javascript para criar objetos instânciando variáveis por através dessa função.
+console.log(`\n5) ${typeof produto}`);
+
+
+
+//CRIANDO OBJECTS COM ESTRUTURAS MAIS COMPLEXAS:
+const carro = { //Veja que temos um carro de uma seguradora com uma série de informações dentro de um único objeto...
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: { //Dentro de um objeto colococamos outro objeto com informações do proprietário...
+        nome: 'Raul',
+        idade: 56,
+        endereco: { //Algumas informações do proprietário também são objetos...
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{ //Veja que possuímos uma chave que contém um array com objetos dentro dele...
+        nome: 'Júnior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 42
+    }],
+    calcularSeguro: function () { //Podemos ter até mesmo funções dentro dos nossos objetos...
+        //...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000; //Veja que em casos onde o objeto é muito grande temos que fazer todo o caminho para acessar...
+carro['proprietario']['endereco']['logradouro'] = 'Av. Gigante'; //Podemos acessar também usando os colchetes e os nomes das chaves em formato de string...
+
+console.log(`\n6)`, carro)
+
+
+
+//ATENÇÃO COM O USO DO DELETE:
+//Quando o usamos o delete em um object apagamos para sempre elementos de dentro de um objeto, e se estes elementos estiverem encadeados, vamos acabar apagando tudo o que estiver encadeado á partir do ponto onde delimitamos o delete...
+delete carro.proprietario.endereco
+console.log(`\n7)`, carro) //Veja que o endereço foi deletado...
+
+delete carro.calcularSeguro
+console.log(`\n8)`, carro) //Agora a função foi deletada...
