fd045eef715ba11146a4c6dd362c861382ca0c64
Gabriel <nobrecoder@gmail.com>
2021-10-19 08:13:02 -0300

    19/10/2021

--- arquivos_das_aulas/034-manipulando_Objects.js
+++ arquivos_das_aulas/034-manipulando_Objects.js
@@ -10,16 +10,30 @@ console.log('1) ',typeof prod1);
 
 
 
+//DECLARANDO OBJECTS LITERALMENTE COM AS NOVAS REGRAS DO ECMASCRIPT 2015:
+//Com as novas regras do ecmascript, se tivermos um valor atrelado a uma variável e desejarmos jogar esses valores dentro de object, tendo como chave o mesmo nome da variável, não precisamos escrever o conjunto chave valor, podemos abreviar colocando somente os nomes das chaves, da seguinte forma:
+const a = 1
+const b = 2
+const c = 3
+
+const objAntigo = {a: 1, b:2, c:3} //Essa era a forma antiga, tínhamos que escrever chave e valor...
+const objEcma = {a, b, c} //Perceba que colocamos somente os nomes das variáveis, e isso já basta...
+
+console.log('\n2)',objAntigo)
+console.log('2)',objEcma) //Perceba que mesmo sem termos explicitado o nome da chave, automáticamente o objeto pegou o nome da chave e o valor...
+
+
+
 //ATRIBUINDO CHAVE E VALOR DINÂMICAMENTE:
 prod1.nome = 'Celular Ultra Mega Power'; //perceba que criamos uma chave "nome" e atribuímos um valor para ela...
 prod1.preco = 4998.90;
-console.log('\n2) ',prod1); //Veja na impressão que temos conjuntos de par chave valor...
+console.log('\n3) ',prod1); //Veja na impressão que temos conjuntos de par chave valor...
 
 
 
 //ATRIBUINDO CHAVES COM ESPAÇO ENTRE AS PALAVRAS:
 prod1['Desconto legal'] = 0.40; //Perceba que, para colocar palavras com espaçamento como chave, tivemos que colocar uma string dentro de colchetes.
-console.log('\n3) ', prod1);
+console.log('\n4) ', prod1);
 
 
 
@@ -36,12 +50,28 @@ const prod2 = { //Perceba que abrimos a chave de dentro colocamos várias chaves
         }
     }
 }
-    console.log('\n4) ',prod2);
+    console.log('\n5) ',prod2);
+
+
+
+//ATRIBUIÇÃO DE NOME DE CHAVE USANDO COLCHETES COM NOVO FORMATO DO ECMASCRIPT 2015:
+//O EcmaScript 2015 possibilitou a criação de chaves á partir de valores existentes dentro de variáveis, podemos fazer isso usando os colchetes, da seguinte forma:
+const chaveEcma = 'Chave'
+const valorEcma = 'Valor'
+
+const objEcmaCol1 = {} //Perceba que já temos o objeto criado...
+objEcmaCol1[chaveEcma] = valorEcma //Veja como usamos os colchetes para atribuir o valor da variável "chaveEcma" e usando diretamente o nome da variável já pegamos o valor dela...
+console.log('\n6)', objEcmaCol1) 
+
+//Podemos também usar esse formato...
+const objEcmaCol2 = {[chaveEcma]: valorEcma} //Perceba que usando apenas uma linha conseguimos pegar o valor da chave e ainda o valor da variável...
+console.log('6)', objEcmaCol2) //Veja que o resultado é o mesmo...
 
 
 
 //RESGATANDO VALORES DE UM OBJECT:
-console.log('\nNome do produto 1: ', prod1.nome); //Para resgatar o valor chamamos pela chave...
+console.log('\n7)');
+console.log('Nome do produto 1: ', prod1.nome); //Para resgatar o valor chamamos pela chave...
 console.log('Preço do produto 1: ', prod1.preco);
 console.log('Desconto do produto 1: ', prod1["Desconto legal"]); //Se a chave tem nome composto usamos os colchetes...
 console.log('\nNome do produto 2: ', prod2.nome);
@@ -53,7 +83,7 @@ console.log('Tamanhos do produto 2: ', prod2.estilo.tamanhos); //Chamando todos 
 
 //DECLARANDO UM OBJECT USANDO A FUNÇÃO OBJECT:
 let produto = new Object; //Veja que podemos usar a função interna Object do javascript para criar objetos instânciando variáveis por através dessa função.
-console.log(`\n5) ${typeof produto}`);
+console.log(`\n8) ${typeof produto}`);
 
 
 
@@ -84,17 +114,28 @@ const carro = { //Veja que temos um carro de uma seguradora com uma série de in
 carro.proprietario.endereco.numero = 1000; //Veja que em casos onde o objeto é muito grande temos que fazer todo o caminho para acessar...
 carro['proprietario']['endereco']['logradouro'] = 'Av. Gigante'; //Podemos acessar também usando os colchetes e os nomes das chaves em formato de string...
 
-console.log(`\n6)`, carro)
+console.log(`\n9)`, carro)
+
+
+
+//INSERINDO FUNÇÕES EM OBJECTS COM O NOVO ECMASCRIPT 2015:
+const objFuncEcma = {
+    funcao: function() {/*...*/} //Essa é a forma convencional de adicionar funções...
+    , 
+    funcao2() {/*...*/} //Essa é a forma nova de adicionar funções no ECMASCRIPT 6, o próprio nome da função já é a sua chave...
+}
+
+console.log('\n10)', objFuncEcma)
 
 
 
 //ATENÇÃO COM O USO DO DELETE:
 //Quando o usamos o delete em um object apagamos para sempre elementos de dentro de um objeto, e se estes elementos estiverem encadeados, vamos acabar apagando tudo o que estiver encadeado á partir do ponto onde delimitamos o delete...
 delete carro.proprietario.endereco
-console.log(`\n7)`, carro) //Veja que o endereço foi deletado...
+console.log(`\n11)`, carro) //Veja que o endereço foi deletado...
 
 delete carro.calcularSeguro
-console.log(`\n8)`, carro) //Agora a função foi deletada...
+console.log(`\n12)`, carro) //Agora a função foi deletada...
 
 
 
@@ -102,10 +143,10 @@ console.log(`\n8)`, carro) //Agora a função foi deletada...
 //Você deve ter percebido que, mesmo quando criamos objetos em constantes ainda assim é possível alterar os valores de suas chaves. Como no exemplo abaixo:
 const pessoa = { nome: 'Gabriel', idade: '30'}
 pessoa.nome = 'Graziela' //Veja que mudamos o nome apesar do objeto estar armazenado numa constante...
-console.log(`\n9) ${pessoa.nome} ${pessoa.idade}`)
+console.log(`\n13) ${pessoa.nome} ${pessoa.idade}`)
 
 //isso acontece por que as constante guardam na verdade um endereço de memória, quando referenciamos ao objeto pessoa, ele na verdade é um endereço de memória que contém uma coleção de dados. Podemos mudar a coleção de dados se quisermos, mas não podemos mudar o endereço de memória jamais...
-pessoa = {nome: 'Gabriel'} //Perceba que temos um erro quando tentamos dar novos valores ao espaço de memória...
+//**** DESCOMENTE AQUI!!! pessoa = {nome: 'Gabriel'} //Perceba que temos um erro quando tentamos dar novos valores ao espaço de memória...
 
 
 
@@ -114,8 +155,8 @@ pessoa = {nome: 'Gabriel'} //Perceba que temos um erro quando tentamos dar novos
 const humano = Object.freeze({nome: 'Gabriel', idade: '30'}) //veja que freeze() deve ser utilizado sempre com conjunto com a função "Object", para que possamos criar um objeto que jamais poderá ser modificado...
 
 humano.nome = 'Graziela' //Veja que embora tenhamos mudado o nome como no exemplo mais acima, com o freeze() o nome sempre será igual ao que foi declarado desde o ínicio...
-console.log(`\n10) ${humano.nome} ${humano.idade}`)
+console.log(`\n14) ${humano.nome} ${humano.idade}`)
 
 delete humano.nome //Não podemos nem mesmo deletar nenhum valor...
-console.log(`\n11)`, humano)
+console.log(`\n15)`, humano)
 //O que for criado com freeze() só poderá ser deletado diretamente no código fonte...
