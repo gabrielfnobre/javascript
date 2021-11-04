e563aa08963be7c58b4260338095867930e9cb20
Gabriel <nobrecoder@gmail.com>
2021-11-04 12:13:33 -0300

    04/11/2021

---
+++ EXERCICIOS_02/EX-23.js
@@ -0,0 +1,7 @@
+const quantasPalavras = string => {
+    let arrayString = string.split(' ')
+    return console.log(arrayString.length)
+}
+
+quantasPalavras("Sou uma frase")
+quantasPalavras("Me divirto aprendendo a programar")
