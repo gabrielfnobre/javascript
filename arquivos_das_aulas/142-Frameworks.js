faffaf295cb778cfea367d176a8f015b42fdd688
Gabriel <nobrecoder@gmail.com>
2021-11-18 12:00:59 -0300

    18/11/2021

---
+++ arquivos_das_aulas/142-Frameworks.js
@@ -0,0 +1,21 @@
+//FRAMEWORKS:
+
+//O que são frameworks?
+//Framework pode ser traduzido literalmente por "estrutura", e essa é a ideia de framework mesmo, diferente das bibliotecas que trazem uma série de funcionalidades que podem ser implementadas no nosso código, os frameworks tem o papel de trazer uma estruturalização do nosso código, trazendo um modelo de projeto para que possamos desenvolver.
+
+//Os frameworks facilitam a vida do desenvolvedor, pois ele já traz uma série de códigos pré-preparados para as mais diversas funcionalidades. Isso faz com que o desenvolvedor dedique menos tempo codificando páginas e trabalhando responsividade, e se dedique mais ao trabalho de desenvolver as funcionalidades de uma aplicação. 
+
+//Para o Javascript temos uma série de frameworks dependendo da utilização, por exemplo, temos frameworks que são especiais para o Front End, trabalhando na diagramação da página e componentes, mas também temos frameworks que são para o Back End, trabalhando no tratamento de dados, querys e persistência destes.
+
+//Alguns exemplos de Frameworks para Javascript são:
+//  Para o Back-End:
+//                  * Express;
+//                  * Sails;
+//                  * AdonisJs;
+//                  * Koa;
+
+//  Para o Front-End:
+//                  * Angular;
+//                  * Vue;
+//                  * Ember;
+//                  * React (Embora muitos não considerem o React um framework e sim uma biblioteca);
