5dcbec2cf7889e0c258116a386d68c5927c6db84
Gabriel <nobrecoder@gmail.com>
2022-02-10 15:13:26 -0300

    10/02/2022

---
+++ arquivos_das_aulas/173-classList___Vendo_Todas_as_Classes_Dentro_de_Um_Elemento.js
@@ -0,0 +1,10 @@
+//classList
+/* 
+    A função classList é uma função de document onde é possível nós termos acesso as todas as classes de um determinado elemento.
+
+    Essas classes são armazenadas dentro de uma lista. O clasList é muito útil também para quando desejamos adicionar elementos.
+
+    Veja no console o resultado da execução de 173-clasList.html, você verá que as 2 classes do elemento são referenciadas...
+*/
+
+console.log(document.querySelector('div').classList)
