267abe6d2755ab5f22ac9f149866595942c0e600
Gabriel <nobrecoder@gmail.com>
2022-02-10 15:45:54 -0300

    10/02/2022

---
+++ arquivos_das_aulas/174-add___Adicionando_Valores_a_Sets.js
@@ -0,0 +1,12 @@
+//add
+/* 
+    A função add() é uma função que deve ser usada em elementos que são sets - conjuntos de valores iteráveis como os arrays - para adicionar valores aos sets.
+
+    Podemos usar também junto a alguns elementos que geram sets, como o "classList" por exemplo, que gera um set com os nomes das classes de um elemento, podendo adicionar uma nova classe a um elemento. 
+
+    Podemos ver isso no exemplo abaixo...
+*/
+
+document.querySelector('div').classList.add('nova_classe')
+
+console.log(document.querySelector('div').classList)
